c1e84049900049d4c4097c0aa08c4a1bc5f686f9
krishnaReddyQa9 <160380101+krishnaReddyQa9@users.noreply.github.com>
2024-10-28 18:47:56 +0530

    Add files via upload
    Signed-off-by: krishnaReddyQa9 <160380101+krishnaReddyQa9@users.noreply.github.com>

--- script.js
+++
@@ -1,6 +0,0 @@
-function changeLanguage(lang) {
-    const elements = document.querySelectorAll('[data-en], [data-te]');
-    elements.forEach(el => {
-        el.textContent = el.getAttribute(`data-${lang}`);
-    });
-}
